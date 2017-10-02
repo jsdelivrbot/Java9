ebe36442aa90e548006215cf697a88ebe84b8a0a
Piotr Grota <piotrgrota@gmail.com>
2017-10-02 12:01:30 +0200

    init java 9 commit

---
+++ interfaces/src/java/main/IDependency.java
@@ -0,0 +1,13 @@
+package main;
+
+@FunctionalInterface
+public interface IDependency {
+    private String testPrivateMethod(){
+        return "Private method";
+    }
+
+    String sayHello();
+
+    //String sayHello(String a);
+     //int test(int a);
+}
---
+++ interfaces/src/java/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Main {
+    public static void main(String [] args){
+        System.out.print("Java 9");
+        test();
+
+    }
+    private static void test(){
+        IDependency dependency = () -> "data";
+        System.out.print(dependency.sayHello());
+    }
+}
