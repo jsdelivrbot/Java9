15645ba4fe0bbdd6ebe81486ab8b1e6b622f4f06
Piotr Grota <piotrgrota@gmail.com>
2017-10-24 22:00:07 +0200

    init build directory

--- modules_basics/build.ps1
+++ modules_basics/build.ps1
@@ -1,15 +1,34 @@
-mkdir -Path output\lib -Force
 
-$classA = "output\classesA"
+mkdir -Path output -Force
+mkdir -Path output\moduleA\lib -Force
+
+$classA = "output\moduleA\classesA"
+$classB = "output\moduleB\classesB"
 
 mkdir -Path $classA -Force
+mkdir -Path $classB -Force
 
 $jav_path = Join-Path  $env:JAVA_HOME  "bin"
 $javac_path = Join-Path  $jav_path "javac.exe"
 $jar_path = Join-Path  $jav_path "jar.exe"
 
 
-
-
+# Compile module A
 &($javac_path) -d $classA $(Get-ChildItem .\com.module.a  -Recurse -Filter '*.java' -File | %{ $_.FullName})
+
+#package module A
+
+&($jar_path)  -c -f "output\moduleA\lib\com.module.a.jar" -C "output\ModuleA\classesA" .
+
+
+
+# Compile module B
+&($javac_path) -d $classB $(Get-ChildItem .\com.module.user  -Recurse -Filter '*.java' -File | %{ $_.FullName})
+
+#packages module B
+&($jar_path)  -c -f "output\moduleB\lib\com.module.user.jar" -C "output\ModuleB\classesB" .
+
+
+
+
 
