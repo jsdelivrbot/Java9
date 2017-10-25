91dff133d72340697c9e3685857666cd3b1c9f00
Piotr Grota <piotrgrota@gmail.com>
2017-10-25 19:46:52 +0200

    add client modules

---
+++ modules_basics/com.module.client/com/module/client/User.java
@@ -0,0 +1,9 @@
+package com.module.client;
+import com.module.a.*;
+
+public class User {
+    public static void main(String[] args){
+        Sample sample = new Sample();
+        System.out.println(sample.moduleA());
+    }
+}
