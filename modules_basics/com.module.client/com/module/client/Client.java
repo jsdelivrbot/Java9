48af5efd2de1b6c8401c16ba5a37f49bc19d108f
Piotr Grota <piotrgrota@gmail.com>
2017-10-25 19:49:18 +0200

    rename modules

--- modules_basics/com.module.client/com/module/client/Client.java
+++ modules_basics/com.module.client/com/module/client/Client.java
@@ -1,7 +1,7 @@
 package com.module.client;
 import com.module.a.*;
 
-public class User {
+public class Client {
     public static void main(String[] args){
         Sample sample = new Sample();
         System.out.println(sample.moduleA());
